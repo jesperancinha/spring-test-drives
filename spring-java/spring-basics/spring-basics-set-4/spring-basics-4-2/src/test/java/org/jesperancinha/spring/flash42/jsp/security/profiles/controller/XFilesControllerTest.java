ee0896b80eccde6214d2d0c5dd3d88cbbb5fed1b
jesperancinha <jofisaes@gmail.com>
2026-08-28 16:47:53 +0200

    Fixes test 42

--- spring-java/spring-basics/spring-basics-set-4/spring-basics-4-2/src/test/java/org/jesperancinha/spring/flash42/jsp/security/profiles/controller/XFilesControllerTest.java
+++ spring-java/spring-basics/spring-basics-set-4/spring-basics-4-2/src/test/java/org/jesperancinha/spring/flash42/jsp/security/profiles/controller/XFilesControllerTest.java
@@ -1,6 +1,7 @@
 package org.jesperancinha.spring.flash42.jsp.security.profiles.controller;
 
 import org.jesperancinha.spring.flash42.jsp.security.profiles.configuration.XFilesConfigurationAdapter;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
@@ -8,9 +9,12 @@ import org.springframework.context.annotation.Import;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Collections;
 
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -20,6 +24,14 @@ class XFilesControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @BeforeEach
+    void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+    }
 
     @Test
     @WithMockUser(username = "joao")
