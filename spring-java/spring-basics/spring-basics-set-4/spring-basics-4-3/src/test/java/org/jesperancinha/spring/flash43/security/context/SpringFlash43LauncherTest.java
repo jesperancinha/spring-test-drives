f045db8a42d4aa273dbf7136599c21cd24e2e676
jesperancinha <jofisaes@gmail.com>
2026-08-28 16:48:49 +0200

    Fixes test 43

--- spring-java/spring-basics/spring-basics-set-4/spring-basics-4-3/src/test/java/org/jesperancinha/spring/flash43/security/context/SpringFlash43LauncherTest.java
+++ spring-java/spring-basics/spring-basics-set-4/spring-basics-4-3/src/test/java/org/jesperancinha/spring/flash43/security/context/SpringFlash43LauncherTest.java
@@ -1,5 +1,6 @@
 package org.jesperancinha.spring.flash43.security.context;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
@@ -7,8 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -21,6 +25,14 @@ class SpringFlash43LauncherTest {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @BeforeEach
+    void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+    }
 
     @Test
     void testLoginPageWhenNoAuthenticationThenAllow() throws Exception {
